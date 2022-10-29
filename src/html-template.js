3416c0c0d54e1dbc3e721a7f8b58d1fe59baae7a
bhaas412 <bhaas412@gmail.com>
2022-10-29 15:34:40 -0400

    Built classes and outlined code

---
+++ src/html-template.js
@@ -0,0 +1,14 @@
+// This is helper code for generating the HTML that's going to be output to an .html file
+
+// generateHTML function accepts a team input which is an array of objects
+
+  // defaultHTML function will generate the default "Employee" HTML that'll handle the name, id, and email
+
+  // engineerHTML function adds GitHub HTML part
+
+  // managerHTML function adds office number HTML part
+
+  // internHTML function adds school HTML part
+
+// Function that generates the HTML page
+// Export this function
